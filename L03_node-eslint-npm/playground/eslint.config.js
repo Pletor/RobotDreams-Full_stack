eaefe57d06345b7e90257315ed96146ffe4986fc
Martin Hejl <martinhejl@yahoo.com>
2025-07-30 17:01:43 +0200

    Lekce 3: Přidání složky L03 se složkou playground s ESLint konfigurací a index.js

---
+++ L03_node-eslint-npm/playground/eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+    js.configs.recommended,
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        languageOptions: {
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly'
+            },
+        },
+        rules: {
+            '@stylistic/indent': ['error', 2],
+            '@stylistic/quotes': ['error', 'single'],
+            '@stylistic/semi': ['error', 'always']
+        }
+    }
+];
